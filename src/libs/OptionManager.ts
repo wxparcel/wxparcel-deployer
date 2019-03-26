68e02e6e791612c972292aa843959757568e5373
DavidJones <qowera@qq.com>
2019-03-26 17:49:18 +0800

    添加

--- src/libs/OptionManager.ts
+++ src/libs/OptionManager.ts
@@ -1,20 +1,36 @@
 import * as path from 'path'
 import { v4 as uuid } from 'uuid'
 import trimEnd = require('lodash/trimEnd')
-import { WXOptions } from '../types'
+import { WXParcelOptions } from '../types'
 
 export default class OptionManager {
+  public uid: string
   public rootPath: string
   public tempPath: string
-  public uid: string
-  public server: string
-  public repository: string
+  public devToolServer: string
+  public deployServerPort: number
+  public maxFileSize: number
 
-  constructor (options: WXOptions) {
-    this.rootPath = path.join(__dirname, '../../')
-    this.tempPath = path.join(this.rootPath, './.temporary')
+  constructor (options: WXParcelOptions) {
     this.uid = uuid()
-    this.server = trimEnd(options.server, '/')
-    this.repository = options.repository
+    this.rootPath = process.cwd()
+    this.tempPath = path.join(this.rootPath, './.temporary')
+    this.maxFileSize = 1024 * 1024 * 8
+
+    this.configure(options)
+  }
+
+  public configure (options: WXParcelOptions) {
+    if (options.hasOwnProperty('deployServerPort')) {
+      this.deployServerPort = options.deployServerPort
+    }
+
+    if (options.hasOwnProperty('devToolServer')) {
+      this.devToolServer = trimEnd(options.devToolServer, '/')
+    }
+
+    if (options.hasOwnProperty('maxFileSize')) {
+      this.maxFileSize = options.maxFileSize
+    }
   }
 }
